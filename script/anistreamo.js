08f56dede762054de24d44e65f8bea46ff586380
KizuKoFaith <134709913+KizuKoFaith@users.noreply.github.com>
2023-08-31 06:57:57 +0800

    update script/anistreamo.js

--- script/anistreamo.js
+++ script/anistreamo.js
@@ -1,8 +1,13 @@
 fetch('https://api.jikan.moe/v4/seasons/now')
 .then(function (response){
   let maindiv = document.getElementsByClassName('carousel-inner');
+  var parseJSON = JSON.parse(this.data);
   
-  
+  for(let item in parseJSON){
+    
+    alert(parseJSON[item].title);
+    
+  }
   
 })
 .catch(function (err){
