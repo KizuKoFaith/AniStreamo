a0a1da08b1317d0f28bf0b5fc3d3da162eb554df
KizuKoFaith <134709913+KizuKoFaith@users.noreply.github.com>
2023-08-31 18:54:16 +0800

    update script/anistreamo.js

--- script/anistreamo.js
+++ script/anistreamo.js
@@ -8,12 +8,16 @@ fetch('https://api.jikan.moe/v4/seasons/now')
   // const jsonToString = JSON.stringify(anime.data);
   
   for(let i in anime.data){
-    let coverImage = anime[i].images;
-    for(let x in coverImage){
-      var cover = coverImage[x].image_url;
-      console.log(cover);
+    let coverImage = anime.data[i].images;
+    //console.log(coverImage.jpg)
+    for(let j in coverImage.jpg){
+      // console.log(coverImage.jpg[j]);
+      let string = "<div class='carousel-item active'><img class='d-block w-100' src='"+coverImage.jpg[j]+"'alt=''/>";
+      
+      maindiv.appendChild(string);
+      
     }
-    // let string = "<div class='carousel-item active'><img class='d-block w-100' src='"+cover alt=""/>"
+   
   }
   
 })
